2861e8f2508e1e6286f7c0018db22b5f99e0ea62
Doxy <paradoxshorizon@gmail.com>
2023-01-07 13:14:34 -0800

    Stream Base on Core
    Remove everything not in dl::dlcore and add a dependency on it
    Only Include Core If Not Already Included!

--- src/testing.cpp
+++
@@ -1,7 +0,0 @@
-// Include the benchmarking implementation code
-#define ANKERL_NANOBENCH_IMPLEMENT
-#define DOCTEST_CONFIG_IMPLEMENT
-#include <testing>
-
-// Memory backing for string used to set the category of each benchmark
-std::string dl::benchmarkTitle = "benchmark";
