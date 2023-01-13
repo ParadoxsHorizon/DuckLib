bc670eb5c6f800434996565d9193ecc6b063260a
Doxy <paradoxshorizon@gmail.com>
2023-01-13 13:17:27 -0800

    Add std::term::reset
    Which invokes every other type of terminal reset!

--- tests.cpp
+++ tests.cpp
@@ -15,12 +15,12 @@ using namespace dl::literals;
 	defer { std::cdbg.rdbuf(std__cout_buf); };\
 
 TEST_CASE("Terminal colors") {
-	std::cout << std::term::info{} << "Hello World!" << std::term::style::reset << std::endl;
-	std::cout << std::term::warning{} << "Hello World!" << std::term::style::reset << std::endl;
-	std::cout << std::term::error{} << "Hello World!" << std::term::style::reset << std::endl;
-	std::cout << std::term::fatal{} << "Hello World!" << std::term::style::reset << std::endl;
-	std::cout << std::term::debug{} << "Hello World!" << std::term::style::reset << std::endl;
-	std::cout << std::term::fg::red << "Hello World!" << std::term::style::reset << std::endl;
+	std::cout << std::term::info{} << "Hello World!" << std::term::reset << std::endl;
+	std::cout << std::term::warning{} << "Hello World!" << std::term::reset << std::endl;
+	std::cout << std::term::error{} << "Hello World!" << std::term::reset << std::endl;
+	std::cout << std::term::fatal{} << "Hello World!" << std::term::reset << std::endl;
+	std::cout << std::term::debug{} << "Hello World!" << std::term::reset << std::endl;
+	std::cout << std::term::fg::red << "Hello World!" << std::term::reset << std::endl;
 }
 
 TEST_CASE("std::cdbg") {
